909ebde0fa4c0aa95d387ea6adf243d838251081
Jonathan Martz <git@jonathan-martz.de>
2024-11-25 15:23:43 +0100

    fix migration settings

--- 1732543741_pocketstore_settings.js
+++ 1732543741_pocketstore_settings.js
@@ -1,10 +1,10 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
+migrate((app) => {
   const settings = app.settings()
   settings.meta.appName = "PocketStore.io - Demo"
   settings.logs.maxDays = 1
 
   app.save(settings)
-}, (db) => {
+}, (app) => {
   // add down queries...
 })
