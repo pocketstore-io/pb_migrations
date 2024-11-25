9efcdc20e64108f7a8c1635d9eac7cd1ef64ecbf
Jonathan Martz <git@jonathan-martz.de>
2024-11-25 15:57:55 +0100

    fix app to db, change appUrl

--- 1732543741_pocketstore_settings.js
+++ 1732543741_pocketstore_settings.js
@@ -1,10 +1,10 @@
 /// <reference path="../pb_data/types.d.ts" />
-migrate((db) => {
+migrate((app) => {
   const settings = app.settings()
   settings.meta.appName = "PocketStore.io - Demo"
+  settings.meta.appURL = "https://demo.pocketstore.io"
   settings.logs.maxDays = 1
-
   app.save(settings)
-}, (db) => {
+}, (app) => {
   // add down queries...
 })
